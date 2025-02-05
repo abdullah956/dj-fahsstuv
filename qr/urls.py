dd16a59a59a3733cbf95a10cbbec904aae4acdfc
abdullah956 <abdullaharshed956@gmail.com>
2025-02-05 16:25:11 +0500

    auth

--- qr/urls.py
+++ qr/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import certificate_form_view, certificate_list_view, download_certificate_pdf
+from .views import certificate_form_view, certificate_list_view, download_certificate_pdf,login_view, signup_view, logout_view
 
 urlpatterns = [
     path('', certificate_form_view, name='certificate_form'),
     path('certificates/', certificate_list_view, name='certificate_list'),
     path('download/<int:pk>/', download_certificate_pdf, name='download_certificate_pdf'),
+    path("login/", login_view, name="login"),
+    path("signup/", signup_view, name="signup"),
+    path("logout/", logout_view, name="logout"),
 ]
--- qr/views.py
+++ qr/views.py
@@ -71,3 +71,34 @@ def download_certificate_pdf(request, pk):
         return HttpResponse("Error generating PDF", status=500)
     
     return response
+
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "users/signup.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
